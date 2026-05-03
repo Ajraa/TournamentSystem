daac5eef538868f519c9f7e4aed72a134c904117
Ajra <salajkapavel@gmail.com>
2026-05-03 18:07:07 +0200

    Přidání env

--- TournamentSystem/settings.py
+++ TournamentSystem/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--+$34)ijw4bg_+kysrx31)#z3k0bg+$suo&l)u0ztb=v_70ngs'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure--+$34)ijw4bg_+kysrx31)#z3k0bg+$suo&l)u0ztb=v_70ngs')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
